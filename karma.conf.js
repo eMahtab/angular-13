6f5f95c4d1a5dc9289e005066f4fda91b39f3d57
Mahtab Alam <Mahtab.Alam@ge.com>
2022-07-26 11:51:48 +0530

    karma watch false and use ChromeHeadless

--- karma.conf.js
+++ karma.conf.js
@@ -36,7 +36,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
+    autoWatch: false,
+    browserNoActivityTimeout: 120000,
+    captureTimeout: 240000,
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
