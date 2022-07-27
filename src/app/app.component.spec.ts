3ba2fafea4aa6576e2a927f9feb36a5bf5a0d882
Mahtab Alam <Mahtab.Alam@ge.com>
2022-07-27 11:14:24 +0530

    changed tesconfig

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { NMViewerComponent } from './nmViewer/nmViewer.component';
+
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        NMViewerComponent
       ],
     }).compileComponents();
   });
